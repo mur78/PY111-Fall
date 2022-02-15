0cbbb668dca6f0735d7b61755aacf18491131c00
Mur78 <drivem78@mail.ru>
2022-02-15 15:38:34 +0300

    commit a2_priority_queue 15.02

--- Tasks/a2_priority_queue.py
+++ Tasks/a2_priority_queue.py
@@ -4,11 +4,14 @@ Priority Queue
 Queue priorities are from 0 to 10
 """
 from typing import Any
+from collections import deque
 
 
 class PriorityQueue:
     def __init__(self):
         # todo для очереди можно использовать python dict
+        self.my_priority_queue = deque()
+
 
     def enqueue(self, elem: Any, priority: int = 0) -> None:
         """
@@ -33,27 +36,49 @@ class PriorityQueue:
         #     },
         # ]
 
+        a = [i for i in range(elem)]
+        for i in a:
+            self.my_priority_queue.insert(i, priority)
+
+        b = []
+        for v in a:
+            b.append(self.my_priority_queue.popleft())
+
+        self.my_priority_queue.insert(0, elem)
+        return None
+
+
+
     def dequeue(self) -> Any:
         """
-        Return element from the beginning of the queue. Should return None if not elements.
+        Return element from the beginning of the queue. Should return None if no elements.
 
         :return: dequeued element
         """
-        return None
+        if not self.my_priority_queue:
+            return None
+        return self.my_priority_queue.pop()  # O(1)
 
-    def peek(self, ind: int = 0, priority: int = 0) -> Any:
+    def peek(self, ind: int = 0) -> Any:
         """
         Allow you to see at the element in the queue without dequeuing it
 
         :param ind: index of element (count from the beginning)
         :return: peeked element
         """
-        return None
+
+        if ind <= len(self.my_priority_queue):
+            peek_val = self.my_priority_queue[-1 - ind]
+            return peek_val
+        else:
+            return None
 
     def clear(self) -> None:
         """
         Clear my queue
-
         :return: None
         """
+        self.my_priority_queue.clear()
+
         return None
+
