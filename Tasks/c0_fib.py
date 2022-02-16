2c7a8c6770dfec493c7cacca5b80e04ee961613e
Mur78 <drivem78@mail.ru>
2022-02-16 18:49:03 +0300

    commit c0_fib 16.02

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -5,8 +5,17 @@ def fib_recursive(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
+    if n < 0:
+        raise ValueError
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib_recursive(n-1) + fib_recursive(n-2)
+
+
+    # print(n)
+    # return 0
 
 
 def fib_iterative(n: int) -> int:
@@ -16,5 +25,18 @@ def fib_iterative(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
+    if n < 0:
+        raise ValueError
+    a = 0
+    b = 1
+    sum = 0
+
+    while n != 1:
+        sum = a + b
+        a, b = b, sum
+        n -= 1
+    return sum
+
+
+    # print(n)
+    # return 0
