5a3b28220905a3327d1a3725e252af94d656b7eb
Mur78 <drivem78@mail.ru>
2022-02-24 18:02:36 +0300

    commit c2_test_recursive_bin_search 24.02

--- Tasks/c2_recursive_binary_search.py
+++ Tasks/c2_recursive_binary_search.py
@@ -9,5 +9,23 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
-    print(elem, arr)
-    return None
+    cur_ind = len(arr) // 2
+    if not arr:
+        return None
+    elif len(arr) == 1:
+        return cur_ind if arr[cur_ind] == elem else None
+    elif arr[cur_ind] == elem:
+        while arr[cur_ind - 1] == elem:
+            cur_ind -= 1
+        return cur_ind
+    else:
+        if elem < arr[cur_ind]:
+            return binary_search(elem, arr[:cur_ind])
+        elif elem > arr[cur_ind]:
+            r_binary = binary_search(elem, arr[cur_ind:])
+            return cur_ind + r_binary if r_binary is not None else None
+
+
+
+    # print(elem, arr)
+    # return None
