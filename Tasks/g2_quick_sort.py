a499a29e41ed4a8964c9616376ea73faa3d2fce4
Mur78 <drivem78@mail.ru>
2022-03-12 16:32:24 +0300

    commit g2_quick_sort 12.03

--- Tasks/g2_quick_sort.py
+++ Tasks/g2_quick_sort.py
@@ -1,11 +1,29 @@
 from typing import List
-
+from random import choice, randint
 
 def sort(container: List[int]) -> List[int]:
     """
     Sort input container with quick sort
-
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
+    def _sort(left_border, right_border) -> None:
+        if left_border >= right_border:
+            return
+        random_index = randint(left_border, right_border)
+        pivot = container[random_index]
+        i, j = left_border, right_border
+        while i <= j:
+            while container[i] < pivot:
+                i += 1
+            while container[j] > pivot:
+                j -= 1
+            if i <= j:
+                # взаимообмен
+                container[i], container[j] = container[j], container[i]
+                i += 1
+                j -= 1
+        _sort(left_border, j)
+        _sort(i, right_border)
+    _sort(0, len(container)-1)
     return container
